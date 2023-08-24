991088eccbb4dee52523f5dbe4d84a4557a8f8d0
mahesh <maheshthota2136@gmail.com>
2023-08-24 22:28:31 +0530

    Final script

--- environment_creation.sh
+++ environment_creation.sh
@@ -20,7 +20,7 @@ do
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "Created $i Instance: $IP_ADDRESS"
 
-    aws route53 change-resource-record-sets --hosted-zone-id Z05444922ON0WMH1WD9T0 --change-batch '
+    aws route53 change-resource-record-sets --hosted-zone-id Z05444922ON0WMH1WD9T0 --change-batch'
     {
                 "Changes": [{
                 "Action": "CREATE",
@@ -30,6 +30,5 @@ do
                                 "TTL": 300,
                                 "ResourceRecords": [{"Value": "$IP_ADDRESS"}]
                             }}]
-    }
-    '  
+    }'
 done
