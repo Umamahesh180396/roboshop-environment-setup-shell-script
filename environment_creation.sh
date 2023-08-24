e380b6c769de7197accc85fdc58d27e931a4f750
mahesh <maheshthota2136@gmail.com>
2023-08-24 22:22:29 +0530

    Final script

--- environment_creation.sh
+++ environment_creation.sh
@@ -4,6 +4,7 @@ NAMES=("MongoDB-Catalogue-User" "Redis-Cart-User" "MySQL-Shipping" "RabbitMQ-Pay
 INSTANCE_TYPE=""
 IMAGE_ID="ami-03265a0778a880afb"
 SECURITY_GROUP_ID="sg-0e658def150f1a79a"
+DOMAIN_NAME=robomart.cloud
 
 # MySQL-Shipping and MongoDB-Catalogue-User instance type shound be t3.medium, for rest t3.micro
 for i in "${NAMES[@]}"
@@ -18,4 +19,17 @@ do
     echo "Creating $i Instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "Created $i Instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z05444922ON0WMH1WD9T0 --change-batch '
+    {
+                "Changes": [{
+                "Action": "CREATE",
+                            "ResourceRecordSet": {
+                                "Name": "'$i."$DOMAIN_NAME'",
+                                "Type": "A",
+                                "TTL": 300,
+                                "ResourceRecords": [{"Value": "'$IP_ADDRESS'"}]
+                            }}]
+    }
+    '    
 done
