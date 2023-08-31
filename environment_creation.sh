38e37c15d75d4dbcc3eb0aa730e48edf9cd4b387
mahesh <maheshthota2136@gmail.com>
2023-08-31 17:04:28 +0530

    updated

--- environment_creation.sh
+++ environment_creation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payments" "web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payments" "web" "dispatch")
 INSTANCE_TYPE=""
 IMAGE_ID="ami-03265a0778a880afb"
 SECURITY_GROUP_ID="sg-0e658def150f1a79a"
@@ -19,7 +19,7 @@ do
     echo "Creating $i Instance"
     IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
     echo "Created $i Instance: $IP_ADDRESS"
-
+    
     aws route53 change-resource-record-sets --hosted-zone-id Z05444922ON0WMH1WD9T0 --change-batch '
     {
                 "Changes": [{
